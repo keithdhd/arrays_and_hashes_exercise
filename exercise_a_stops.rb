d8cea5dca6c430dcd389f0b4987abdb5ef2abe81
Keith Douglas <keith.douglas@codeclan.com>
2018-11-29 09:46:21 +0000

    number 5 done

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -16,6 +16,8 @@ puts stops.index("Linlithgow")
 
 
 # 5. Remove `"Livingston"` from the array using its name
+stops.delete("Livingston")
+
 # 6. Delete `"Cumbernauld"` from the array by index
 # 7. How many stops there are in the array?
 # 8. How many ways can we return `"Falkirk High"` from the array?
